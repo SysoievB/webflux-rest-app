0dabd2436b870940090f6d0d66748fcfc9a3df55
SysoievB <sysoiev.bogdan.ua@gmail.com>
2023-04-02 09:12:56 +0200

    Initial commit

---
+++ src/main/java/com/webflux/webfluxrestapp/WebfluxRestAppApplication.java
@@ -0,0 +1,13 @@
+package com.webflux.webfluxrestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxRestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxRestAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/webflux/webfluxrestapp/WebfluxRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webflux.webfluxrestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxRestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
